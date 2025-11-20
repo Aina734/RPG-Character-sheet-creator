4dd5e446875fa56dd16c4875dd19adeab4d355c3
Aina734 <82905910+Aina734@users.noreply.github.com>
2025-11-20 17:39:05 +0000

    express conection test running on local host correctly

--- server/server.js
+++ server/server.js
@@ -2,4 +2,28 @@
 
 const express = require("express");
 const dotenv = require("dotenv"); 
-//Load .env variables 
+
+//Load .env variables
+
+//run express function 
+
+const app = express();
+
+// set a hard coded port that will be changed by using .env
+
+const PORT = 3001;
+
+console.log(`starting our test server on ${PORT}`);
+
+//Our routes to the server
+app.get("/api/test", (req, res)=>{
+console.log(`GET request received from /`);
+res.json({status:"ok"});
+});
+
+//start the server
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port: ${PORT}`);
+    console.log(`visit http://localhost:3001/api/test to check server`);
+})
