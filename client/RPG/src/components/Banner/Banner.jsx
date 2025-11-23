acca3914a3e08297e87b1d4c7ed2063e61c67794
Aina734 <82905910+Aina734@users.noreply.github.com>
2025-11-23 18:50:47 +0000

    Banner component added

--- client/RPG/src/components/Banner/Banner.jsx
+++ client/RPG/src/components/Banner/Banner.jsx
@@ -5,6 +5,7 @@ function Banner(){
         <h1>Welcome to RPG Character Creator</h1>
 
         <h2>Let your Creativity run Wild!</h2>
+        
         <section>
             <p>Here you will be able to create your character for your next campaign!</p>
             <p>The Character creator is based on DnD Character Sheet</p>
